abbfdb4b74192936c746c8d6ac6bd2aafaf63400
martinsj438 <169121742018.3@senacrjedu.local>
2019-07-19 12:12:14 -0300

    qualquer mensagem

--- src/app/pages/maps/maps.page.ts
+++ src/app/pages/maps/maps.page.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
+
 
 @Component({
   selector: 'app-maps',
@@ -9,34 +20,51 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 })
 export class MapsPage implements OnInit {
 
-  
-    lat:number
-    lng:number
-  
+
+  lat: number
+  lng: number
+  map: GoogleMap;
+
 
   constructor(
     private geolocation: Geolocation
   ) { }
 
   ngOnInit() {
+    this.loadMap();
   }
 
-  posLocal(){
-  this.geolocation.getCurrentPosition().then((resp) => {
-    this.lat  = resp.coords.latitude;
-    this.lng = resp.coords.longitude;
-    console.log(this.lat, " ", this.lng);
-   }).catch((error) => {
-     console.log('Error getting location', error);
-   });
+  posLocal() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat = resp.coords.latitude;
+      this.lng = resp.coords.longitude;
+      console.log(this.lat, " ", this.lng);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
   }
 
   posAtual() {
-   let watch = this.geolocation.watchPosition();
-   watch.subscribe((data) => {
-    // data can be a set of coordinates, or an error (if an error occurred).
-    // data.coords.latitude
-    // data.coords.longitude
-   });
-}
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+    });
+  }
+
+  loadMap() {
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+  }
 }
